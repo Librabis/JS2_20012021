2b2ee23f9b3405476c2e74dd48e7077717a8afdd
Natalia Murysina <librabisss@gmail.com>
2021-01-27 00:39:26 +0300

    add

--- project/js/main.js
+++ project/js/main.js
@@ -2,7 +2,7 @@ const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 4, title: 'Gamepad', price: 500},
 ];
 //Функция для формирования верстки каждого товара
 const renderProduct = (products) => {
